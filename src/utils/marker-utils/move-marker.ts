d258e44a2687e17ef042a73fed33553502a9c445
Gil Neto <gilneto8@gmail.com>
2020-06-19 18:11:55 +0100

    minor progress.

--- src/utils/marker-utils/move-marker.ts
+++ src/utils/marker-utils/move-marker.ts
@@ -1,6 +1,7 @@
 import { MarkerObj } from '@logic/features/trip/marker-types';
 import { LineObj } from '@logic/features/trip/line-types';
 import { DropResult } from 'react-beautiful-dnd';
+import * as _ from 'lodash';
 
 type GeometryObj = {
   markers: Array<MarkerObj>;
@@ -8,6 +9,11 @@ type GeometryObj = {
 };
 
 export function moveMarker(dropResult: DropResult, geometry: GeometryObj): GeometryObj {
-  console.log('move', dropResult, geometry);
+  if (!dropResult.destination) return geometry;
+  const sourceMarker = _.find(geometry.markers, (m) => m.id === dropResult.draggableId);
+  const destinationMarker = geometry.markers[dropResult.destination.index];
+  console.log('move...', sourceMarker);
+  console.log('to', destinationMarker);
+  console.log('on', geometry);
   return geometry;
 }
