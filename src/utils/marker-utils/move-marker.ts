5285e88e6d58341621bfb220726d6519a71c3b71
Gil Neto <gilneto8@gmail.com>
2020-06-19 19:06:40 +0100

    minor progress.

--- src/utils/marker-utils/move-marker.ts
+++ src/utils/marker-utils/move-marker.ts
@@ -9,11 +9,26 @@ type GeometryObj = {
 };
 
 export function moveMarker(dropResult: DropResult, geometry: GeometryObj): GeometryObj {
-  if (!dropResult.destination) return geometry;
   const sourceMarker = _.find(geometry.markers, (m) => m.id === dropResult.draggableId);
+  if (!sourceMarker) return geometry;
+  if (!dropResult.destination) return geometry;
   const destinationMarker = geometry.markers[dropResult.destination.index];
-  console.log('move...', sourceMarker);
-  console.log('to', destinationMarker);
-  console.log('on', geometry);
-  return geometry;
+
+  let newMarkers: Array<MarkerObj>;
+  if (sourceMarker.order > destinationMarker.order) {
+    newMarkers = _.map(geometry.markers, (m) => {
+      if (m.order > destinationMarker.order) return { ...m, order: m.order - 1 };
+      if (m.id === destinationMarker.id) return { ...m, order: destinationMarker.order };
+      return m;
+    });
+  } else
+    newMarkers = _.map(geometry.markers, (m) => {
+      if (m.order < destinationMarker.order) return { ...m, order: m.order + 1 };
+      if (m.id === destinationMarker.id) return { ...m, order: destinationMarker.order };
+      return m;
+    });
+  const newGeometry: GeometryObj = { markers: newMarkers, lines: geometry.lines };
+  console.log('geometry', geometry.markers);
+  console.log('newGeometry', newGeometry.markers);
+  return newGeometry;
 }
